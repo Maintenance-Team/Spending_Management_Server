9302eca19064eede32e32ccb0f39cfb67385c687
Hoàng Văn Phúc <20521760@gm.uit.edu.vn>
2023-05-26 16:04:19 +0700

    add some api for group types

---
+++ src/controllers/groupType.controller.js
@@ -0,0 +1,73 @@
+import groupTypeServices from '../services/groupType.services.js';
+
+export default {
+  getAllGroupType: async (req, res, next) => {
+    try {
+      const data = await groupTypeServices.getAllGroupType();
+      res.status(200).json({
+        status: 200,
+        message: 'get all group type success',
+        data,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  getOneGroupType: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const data = await groupTypeServices.getGroupTypeById(id);
+      res.status(200).json({
+        status: 200,
+        message: 'get a group type success',
+        data,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  createGroupType: async (req, res, next) => {
+    try {
+      const { type, groupName } = req.body;
+      const data = await groupTypeServices.createNewGroupType(type, groupName);
+      res.status(201).json({
+        status: 201,
+        message: 'create new group type success',
+        data,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  updateGroupType: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { type, groupName } = req.body;
+      const data = await groupTypeServices.updateGroupType(id, type, groupName);
+      res.status(200).json({
+        status: 200,
+        message: 'update a group type success',
+        data,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  deleteGroupType: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const data = await groupTypeServices.deleteGroupType(id);
+      res.status(200).json({
+        status: 200,
+        message: 'delete a group type success',
+        data,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import userRouter from './user.route.js';
 import authRouter from './auth.route.js';
 import spendRouter from './spend.route.js';
+import groupTypeRouter from './groupType.route.js';
 
 const route = (app) => {
   app.use('/users', userRouter);
   app.use('/auth', authRouter);
   app.use('/spends', spendRouter);
+  app.use('/group-types', groupTypeRouter);
 };
 
 export default route;
